ad3922333a80597dd1813215dccf6d955543d899
Hajime25 <44037068+Hajime25@users.noreply.github.com>
2019-02-02 03:53:58 +0800

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,11 +22,7 @@ PM_START_TEXT = """
 Hi {}, my name is {}! If you have any questions on how to use me, read /help.
 
 I'm a group manager bot maintained by [this thug](tg://user?id={}). You can find what makes me tick \
-[here](github.com/shanuflash/harleyquinnbot)!
-
-Feel free to submit pull requests on github, or to contact [my master](t.me/shanuflash), with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @Jokerharleynews for announcements on new features, downtime, etc.
+[here](https://github.com/Hajime25/BOTKOTOMGATANGA)!
 
 If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
 """
