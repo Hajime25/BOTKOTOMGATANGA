279d7df2e67d9e07a663e3cc6860ec3bb04fe5f1
Hajime25 <44037068+Hajime25@users.noreply.github.com>
2019-02-02 02:34:48 +0800

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -60,7 +60,7 @@ def ban(bot: Bot, update: Update, args: List[str]) -> str:
 
     try:
         chat.kick_member(user_id)
-        bot.send_sticker(chat.id, BAN_STICKER)  # banhammer marie sticker
+        bot.send_video(chat.id, "https://i.imgur.com/htcBJu1.gif")  # banhammer marie sticker
         message.reply_text("Banned!")
         return log
 
